afbd9c53fefe616a70330ddc28835abd0ed6595c
RokasVilcinskasHaven <167087322+RokasVilcinskasHaven@users.noreply.github.com>
2025-03-20 20:02:56 +0200

    home hero

--- src/app/[lang]/page.tsx
+++ src/app/[lang]/page.tsx
@@ -1,14 +1,33 @@
 "use client";
+import { Button } from "@/components/Button";
 import { useI18n } from "@/contexts/I18nContext";
 
 export default function LangHome({ params }: { params: { lang: string } }) {
   const { t } = useI18n();
 
   return (
-    <div>
-      <h1 className="text-[70px] leading-[80px] font-bold mb-20 text-center text-blue">
-        {t("home.heroTitle")}
-      </h1>
-    </div>
+    <section>
+      <div className="flex flex-col items-center max-w-[1212px] mx-auto w-full">
+        <h1 className="text-[70px] leading-[80px] font-bold mb-20 text-center text-blue">
+          {t("home.heroTitle")}
+        </h1>
+        <p className="text-[30px] leading-[40px] text-center mb-20">
+          {t("home.heroDescription")}{" "}
+          <span className="text-blue">“Robodam scan”</span>.
+        </p>
+        <div className="flex gap-4 items-center max-w-[540px] justify-between w-full">
+          <Button
+            href="/en"
+            className="max-w-[195px] w-full"
+            variant="secondary"
+          >
+            {t("common.login")}
+          </Button>
+          <Button href="/en" className="max-w-[195px] w-full">
+            {t("common.register")}
+          </Button>
+        </div>
+      </div>
+    </section>
   );
 }
---
+++ src/components/Button.tsx
@@ -0,0 +1,35 @@
+import { twMerge } from "tailwind-merge";
+
+export interface ButtonAnchorProps {
+  href: string;
+  children: React.ReactNode;
+  size?: "small" | "normal";
+  variant?: "primary" | "secondary";
+  className?: string;
+}
+
+export const Button: React.FC<ButtonAnchorProps> = ({
+  href,
+  children,
+  size = "normal",
+  variant = "primary",
+  className,
+}) => {
+  return (
+    <a
+      href={href}
+      className={twMerge(
+        "flex items-center rounded-[5px] justify-center rounded-lg hover:shadow-md text-center capitalize font-bold",
+        size === "normal"
+          ? "px-4 py-2.5 text-[40px] leading-[49px]"
+          : "px-3 py-1.5",
+        variant === "primary"
+          ? "bg-blue text-white"
+          : "bg-white border-2 border-blue text-blue",
+        className
+      )}
+    >
+      {children}
+    </a>
+  );
+};
