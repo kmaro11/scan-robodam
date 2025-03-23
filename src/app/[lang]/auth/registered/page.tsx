eeb83781db54434a3b204b4f8418c5244251938e
RokasVilcinskasHaven <167087322+RokasVilcinskasHaven@users.noreply.github.com>
2025-03-23 20:51:22 +0200

    fixes

---
+++ src/app/[lang]/auth/registered/page.tsx
@@ -0,0 +1,25 @@
+"use client";
+import { Button } from "@/components/Button";
+import { useI18n } from "@/contexts/I18nContext";
+import { useParams } from "next/navigation";
+
+export default function Registered() {
+  const { t } = useI18n();
+  const params = useParams();
+  const { lang } = params;
+  return (
+    <div className="">
+      <h1 className="text-[60px] leading-[70px] font-bold text-center mt-24">
+        {t("auth.normalText", {
+          blue: <span className="text-blue">{t("auth.blueText")}</span>,
+        })}
+      </h1>
+      <Button
+        href={`/${lang}/auth/login`}
+        className="md:h-20 max-w-[400px] mx-auto w-full mt-24"
+      >
+        {t("common.login")}
+      </Button>
+    </div>
+  );
+}
